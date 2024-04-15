82da1f0170e469d34e9ab826f8a7d5fe741279a1
rahatbekdev <rahatbekmamyrbaev103@gmail.com>
2024-04-15 16:29:17 +0600

    finish home-work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import "./App.css";
 
-import './App.css'
-
+function SayHello() {
+  return <h1>HELLO WORLD</h1>;
+}
 function App() {
-
   return (
     <>
-   <div></div>
+      <div>
+        <SayHello />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
